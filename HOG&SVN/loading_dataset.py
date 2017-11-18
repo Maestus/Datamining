7bd002d61a6e4ff6719f1dfe5602eaf85edf0b06
bensassi <bensassi@informatique.univ-paris-diderot.fr>
2017-11-18 23:47:10 +0100

    chargement d'un dossier train dataset depuis un grande archive

---
+++ HOG&SVN/loading_dataset.py
@@ -0,0 +1,34 @@
+import os
+from shutil import copyfile
+import fnmatch
+
+def get_train_dataset(train_file_nomber) :
+    # TO CHANGE
+    # Download Full dataset : https://s3.amazonaws.com/nist-srd/SD19/by_class.zip (more than 1GB)
+    path = '/Users/bensassi/Downloads/by_class/'
+    path = os.path.normpath(path)
+
+    path = path.rstrip(os.path.sep)
+    assert os.path.isdir(path)
+    num_sep = path.count(os.path.sep)
+
+    target = '../dataset/'
+
+    max_to_read = train_file_nomber
+
+    for root, dirs, files in os.walk(path) :
+        if fnmatch.fnmatch(root, '*train*'):
+            print(root)
+            os.makedirs(target + root.split(os.path.sep)[-1:][0])
+            max_to_read = train_file_nomber
+            for name in files :
+                if(max_to_read > 0):
+                    copyfile(os.path.join(root, name), target + root.split(os.path.sep)[-1:][0] + os.path.sep + name)
+                    max_to_read -= 1
+                else:
+                    break
+        else:
+            continue
+
+
+get_train_dataset(25)
