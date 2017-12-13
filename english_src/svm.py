27605bdb3ea8041188e2c093a63276261189807b
Maestus <bensassi.rached@hotmail.com>
2017-12-13 18:34:14 +0100

    clean et separation dans init_dataset des operations de creation

--- english_src/svm.py
+++ english_src/svm.py
@@ -10,36 +10,39 @@ import numpy as np
 import shutil
 import matplotlib.pyplot as plt
 from loading_dataset import *
+import sys
+from random import shuffle
 
-if not os.path.isfile('classifier.pkl') :
 
-    get_dataset(160)
+print('Number of arguments: ' + str(len(sys.argv)) + ' arguments.')
+print('Argument List: ' + str(sys.argv))
 
-    get_dataset(20, False)
+if len(sys.argv) != 2 :
+    print("agument necessaire: poly ou linear")
+    sys.exit(0)
 
-    images, labels = load_dataset("../training_dataset/")
+if((sys.argv[1] != "linear") and (sys.argv[1] != "poly")) :
+        print("agument necessaire: poly ou linear")
+        sys.exit(0)
 
-    data = images.reshape(len(images), -1)
+images, labels = load_dataset("../training_dataset/", 160)
 
-    classifier = svm.SVC(gamma=0.001)
+data = images.reshape(len(images), -1)
 
-    print("classifier set\n")
-
-    classifier.fit(data, labels)
-
-    print("fitted\n")
-
-    joblib.dump(classifier, 'classifier.pkl')
-
-    print("file dumped\n")
-
+if sys.argv[1] == "poly" :
+    classifier = svm.SVC(kernel='poly', gamma=0.001)
 else :
+    classifier = svm.SVC(kernel='linear', gamma=0.001)
 
-    classifier = joblib.load('classifier.pkl')
+print("classifier set\n")
+
+classifier.fit(data, labels)
+
+print("fitted\n")
 
 print("process prediction")
 
-images_test, labels_test = load_dataset("../test_dataset/", False)
+images_test, labels_test = load_dataset("../test_dataset/", 160, False)
 
 data_test = images_test.reshape(len(images_test), -1)
 
@@ -51,6 +54,8 @@ accuracy = accuracy_score(expected,predicted)
 print("****************** average_score : " + str(accuracy))
 
 images_and_predictions = list(zip(images_test, predicted))
+shuffle(images_and_predictions)
+
 for index, (image, prediction) in enumerate(images_and_predictions[80:120]):
     plt.subplot(2, 20, index + 1)
     plt.axis('off')
