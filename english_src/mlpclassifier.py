62aafe5d6019a13182696a896c32478b899d2395
Maestus <bensassi.rached@hotmail.com>
2017-12-13 18:35:03 +0100

    clean et affichage de quelques predictions

--- english_src/mlpclassifier.py
+++ english_src/mlpclassifier.py
@@ -1,35 +1,42 @@
-# Utilisation de sklearn neural network
-
 import os
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score, classification_report
 from loading_dataset import load_dataset
+import matplotlib.pyplot as plt
+from random import shuffle
 
-if os.path.isdir('../training_dataset/') :
+images_test, labels_test = load_dataset("../training_dataset/", 160)
 
-    images_test, labels_test = load_dataset("../training_dataset/")
+data = images_test.reshape(len(images_test), -1)
 
-    data = images_test.reshape(len(images_test), -1)
+classifier = MLPClassifier(solver="sgd")
 
+classifier.activation = "relu"
+classifier.learning_rate_init = 0.001
+classifier.learning_rate = "adaptive"
 
-    classifier = MLPClassifier(solver="sgd")
+print(classifier)
 
-    classifier.activation = "logistic"
-    classifier.learning_rate_init = 0.001
-    classifier.learning_rate = "adaptive"
-    #classifier.batch_size = 10
+classifier.fit(data, labels_test)
 
-    print(classifier)
+images_test, labels_test = load_dataset("../test_dataset/", 160, False)
 
-    classifier.fit(data, labels_test)
+data_test = images_test.reshape(len(images_test), -1)
 
-    images_test, labels_test = load_dataset("../test_dataset/", False)
+expected = labels_test
+predicted = classifier.predict(data_test)
 
-    data_test = images_test.reshape(len(images_test), -1)
+accuracy = accuracy_score(expected,predicted)
 
-    expected = labels_test
-    predicted = classifier.predict(data_test)
+print("****************** average_score : " + str(accuracy))
 
-    accuracy = accuracy_score(expected,predicted)
+images_and_predictions = list(zip(images_test, predicted))
+shuffle(images_and_predictions)
 
-    print("****************** average_score : " + str(accuracy))
+for index, (image, prediction) in enumerate(images_and_predictions[0:40]):
+    plt.subplot(2, 20, index + 1)
+    plt.axis('off')
+    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    plt.title(str(prediction))
+
+plt.show()
